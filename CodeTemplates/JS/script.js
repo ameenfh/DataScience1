ff33a69d161b20eeedcf5a46dd630d71c0dc5cda
Hanish <ahanish2@cherrycreekschools.org>
2023-11-09 11:43:49 -0700

    Javascript learning

--- CodeTemplates/JS/script.js
+++ CodeTemplates/JS/script.js
@@ -1,1 +1,13 @@
 // JavaScript source code
+
+window.addEventListener('scroll', function() {
+    var image = document.getElementById('picture');
+    var currentScrollPos = window.scrollY;
+
+    if (currentScrollPos > 1000) {
+        image.src = 'rick.jpg';
+        }
+    else if (currentScrollPos < 1000){
+        image.src = 'mars.jpg';
+    }
+});
