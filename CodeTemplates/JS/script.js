181c8f82649fbc7246de257c3edbc16623b3c348
Hanish <ahanish2@cherrycreekschools.org>
2023-11-13 10:29:51 -0700

    Scrolls activating transitions

--- CodeTemplates/JS/script.js
+++ CodeTemplates/JS/script.js
@@ -4,10 +4,13 @@ window.addEventListener('scroll', function() {
     var image = document.getElementById('picture');
     var currentScrollPos = window.scrollY;
 
-    if (currentScrollPos > 1000) {
-        image.src = 'rick.jpg';
+    if (currentScrollPos > 1000 && currentScrollPos <= 2000) {
+        image.src = 'second.png';
         }
     else if (currentScrollPos < 1000){
-        image.src = 'mars.jpg';
+        image.src = 'mars.png';
+    }
+    else if (currentScrollPos > 2000){
+        image.src = 'third.png';
     }
 });
